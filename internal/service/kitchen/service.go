a0392f517f32afc89eb29ab3514a6be5335d0e55
Temutjin2k <tamutdzhin2006@mail.ru>
2025-08-10 14:09:34 +0500

    log

--- internal/service/kitchen/service.go
+++ internal/service/kitchen/service.go
@@ -199,7 +199,7 @@ func (s *KitchenWorker) proccesOrder(ctx context.Context, req *models.CreateOrde
 		RequestID:   requestID,
 	}); err != nil {
 		s.log.Error(ctx, types.ActionRabbitMQPublishFailed, "failed to publish status update", err, "worker-name", s.worker.name)
-		s.log.Warn(ctx, types.ActionRabbitMQPublishFailed, "order has been proccessed, but ")
+		s.log.Warn(ctx, types.ActionRabbitMQPublishFailed, "order has been proccessed, but failed to publish status update")
 		// TODO: Think what to do, return error or continue
 	}
 
